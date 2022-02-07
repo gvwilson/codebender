d956e1134c92a5cb25bff67a86ac6964e4ee1b96
Greg Wilson <gvwilson@third-bit.com>
2022-02-07 05:58:26 -0500

    Moving bibliography into place and getting rid of slide logos

--- new/static/site.js
+++ new/static/site.js
@@ -1,20 +1,8 @@
 /**
- * Add slide logo.
- */
-const addSlideLogo = () => {
-  Array.from(document.querySelectorAll('.remark-slide-content'))
-    .filter(node => !node.classList.contains('title-slide'))
-    .forEach(el => {
-      console.log('ADDING')
-      el.innerHTML += '<div class="slides-logo"></div>'
-    })
-}
-
-/**
  * Find and fix bibliographic citations.
  * @param {string} toRoot Path to root of site.
  */
-const fixBibCites = (toRoot) => {
+const fixCites = (toRoot) => {
   Array.from(document.querySelectorAll('cite'))
     .forEach(node => {
       const keys = node.innerHTML
@@ -30,25 +18,9 @@ const fixBibCites = (toRoot) => {
 }
 
 /**
- * Find and fix glossary references.
- * @param {string} toRoot Path to root of site.
- */
-const fixGlossRefs = (toRoot) => {
-  Array.from(document.querySelectorAll('a'))
-    .filter(node => node.getAttribute('href').startsWith('g#'))
-    .forEach(node => {
-      const key = node.getAttribute('href').replace('g#', '')
-      node.setAttribute('href', `${toRoot}/glossary/#${key}`)
-      node.classList.add('glossary')
-    })
-}
-
-/**
  * Perform all in-page fixes.
  * @param {string} toRoot Path to root of site (default is one level up).
  */
 const fixPage = (toRoot='..') => {
-  addSlideLogo()
-  fixBibCites(toRoot)
-  fixGlossRefs(toRoot)
+  fixCites(toRoot)
 }
