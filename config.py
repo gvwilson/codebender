03ded4c13c7248eb8f0d51e403657fb89383f432
Greg Wilson <gvwilson@third-bit.com>
2022-02-19 07:11:25 -0500

    Converting to Ivy

---
+++ config.py
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------------------------
+# Site Configuration File
+# ------------------------------------------------------------------------------
+
+# Theme (in 'lib').
+theme = "graphite"
+
+# Output directory (must be 'docs' to keep GitHub Pages happy).
+out_dir = "docs"
+
+# Site title.
+title = "Managing Research Software Projects"
+
+# Site tagline (unused in McCole theme).
+# tagline = ""
+
+# Site GitHub URL.
+github = "https://github.com/gvwilson/codebender"
+
+# Site domain (if any).
+domain = "codebender.org"
+
+# Site author.
+author = "Greg Wilson"
